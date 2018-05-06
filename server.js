2135149ccb097272e99472629185ee4b3cfa10d4
KarriKangas <kvjkan@utu.fi>
2018-05-06 12:32:08 +0300

    Complete back-end functionality

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.post('/login', function(req, res) {
 });
 
 function TryConfirmAction(result) {
-    if (result && result.length > 0 || result > -2) {
+    if (result && result.length > 0 || result > -2 || result.id > -1) {
         return true;
     }
 
@@ -109,7 +109,7 @@ app.post('/question', function(req, res) {
             } else if (tokenToFind == result[0].token) {
                 console.log("User found!");
                 searchResult = result[0].id;
-                var questionToFind = Math.floor(Math.random() * Math.floor(3));
+                var questionToFind = Math.floor(Math.random() * Math.floor(48));
                 var questionQuery = {
                     id: questionToFind
                 };
@@ -121,6 +121,7 @@ app.post('/question', function(req, res) {
                         res.send("No question found");
                     } else {
                         res.send(result[0].question);
+                        saveQID(tokenToFind, result[0].id);
                     }
 
                 });
@@ -135,18 +136,125 @@ app.post('/question', function(req, res) {
     console.log("We have success!");
 });
 
-var testAnswer = {
-    category: 1, // (which category)
-    answer: 5, // (what was the answer)
-    weight: 0.5, // (weight of questions answered)
-    previousAnswers: 18, //(Amount of previous answers)
-    category0WBI: 80.0, // (The index of category 0)
-    category1WBI: 80.0, // (The index of category 1)
-    category2WBI: 90.0, // (The index of category 2)
-    category3WBI: 40.0, // (The index of category 3)
-    category4WBI: 56.0, // (The index of category 4)
-    category5WBI: 78.0, // (The index of category 5)
-    totalWBI: 23.1
+function saveQID(tokenToFind, QID) {
+
+    var myquery = {
+        token: tokenToFind
+    }
+
+    var newvalues = {
+        $set: {
+            currentQID: QID
+        }
+    };
+
+
+    MongoClient.connect(url, function(err, db) {
+        var dbo = db.db("wellbeing");
+        dbo.collection("users").find(myquery).toArray(function(err, result) {
+            if (err) throw err;
+
+            console.log("Found " + result[0] + " updating to " + QID);
+            dbo.collection("users").updateOne(myquery, newvalues, function(err, res) {
+                if (err) throw err;
+                console.log("1 document updated");
+
+
+            });
+
+            db.close();
+        });
+    });
+
 }
 
-//console.log(QA.question.new_answer(testAnswer).weight + " and " + QA.question.new_answer(testAnswer).totalWBI + " hmm");
+app.post('/answer', function(req, res) {
+    var tokenToFind = req.body.token;
+    var useranswer = req.body.answer;
+    var myquery = {
+        token: tokenToFind
+    }
+    var question;
+    MongoClient.connect(url, function(err, db) {
+        var dbo = db.db("wellbeing");
+        dbo.collection("users").find(myquery).toArray(function(err, result) {
+            if (err) throw err;
+
+            if (!result[0]) {
+                searchResult = -1;
+            } else if (tokenToFind == result[0].token) {
+                console.log("User found with QID " + result[0].currentQID);
+
+
+                var questionQuery = {
+                    id: result[0].currentQID
+
+                };
+                dbo.collection("questions").find(questionQuery).toArray(function(err, qres) {
+                    if (err) throw err;
+
+                    if (!qres[0]) {
+                        console.log("No question found");
+                    } else {
+                        question = qres[0];
+                        console.log("Question found");
+                    }
+
+                });
+
+                var tryQuestionInterval = setInterval(function() {
+                    questionTimer()
+                }, 1000);
+
+                function questionTimer() {
+                    if (TryConfirmAction(question)) {
+                        console.log("Yes question! " + question.category + " " + question.weight + " " + result[0].wbi + " " + result[0].C0Index);
+                        var answerValues = {
+                            category: question.category, // (which category)
+                            answer: useranswer, // (what was the answer)
+                            weight: question.weight, // (weight of questions answered)
+                            previousAnswers: result[0].list_pre_question, //(Amount of previous answers)
+                            category0WBI: result[0].C0Index, // (The index of category 0)
+                            category1WBI: result[0].C1Index, // (The index of category 1)
+                            category2WBI: result[0].C2Index, // (The index of category 2)
+                            category3WBI: result[0].C3Index, // (The index of category 3)
+                            category4WBI: result[0].C4Index, // (The index of category 4)
+                            category5WBI: result[0].C5Index, // (The index of category 5)
+                            totalWBI: result[0].wbi
+                        }
+
+                        var calculatedAnswer = QA.question.new_answer(answerValues);
+
+                        var newvalues = {
+                            $set: {
+                                wbi: calculatedAnswer.totalWBI,
+                                C0Index: calculatedAnswer.category0WBI,
+                                C1Index: calculatedAnswer.category1WBI,
+                                C2Index: calculatedAnswer.category2WBI,
+                                C3Index: calculatedAnswer.category3WBI,
+                                C4Index: calculatedAnswer.category4WBI,
+                                C5Index: calculatedAnswer.category5WBI,
+                            }
+                        }
+
+                        dbo.collection("users").updateOne(myquery, newvalues, function(err, res) {
+                            if (err) throw err;
+                            console.log("1 document updated");
+
+                        });
+                        clearInterval(tryQuestionInterval);
+                    } else {
+                        console.log("No question!");
+                    }
+                    db.close();
+                }
+
+
+
+                //res.send("Error no token found");
+            }
+
+        });
+    });
+});
+
